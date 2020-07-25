b2792d8669975d15c85a1743f913f7d350347e2d
Flavio fernando da Silva <tchfernando@gmail.com>
2020-07-25 07:44:25 -0300

    error when undefined techs corrigido

--- backend/src/utils/parseStringAsArray.js
+++ backend/src/utils/parseStringAsArray.js
@@ -1,3 +1,8 @@
 module.exports = function parseStringAsArray(arrayAsString) {
+
+    if (!arrayAsString) {
+        return [];
+    }
+    
     return arrayAsString.split(',').map(tech => tech.trim());
 }
